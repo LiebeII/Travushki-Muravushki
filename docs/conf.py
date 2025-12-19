3f0e30a951b084ee28d311c6fd7d14c43997e414
Liebe <fedortk65@gmail.com>
2025-12-19 08:09:53 +0300

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-project = 'Анализатор клавиатурных раскладок'
-copyright = '2024, Ваше имя или организация'
-author = 'Ваше имя'
+project = 'Keyboard Layout Analyzer v2.0'
+copyright = '2024, Your Name or Organization'
+author = 'Your Name'
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.autosummary',
 ]
 
 templates_path = ['_templates']
@@ -15,3 +15,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 language = 'ru'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Добавляем поддержку emoji
+html_show_sourcelink = False
+html_theme_options = {
+    'display_version': True,
+    'style_external_links': True,
+}
